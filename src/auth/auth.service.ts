a6960f9c8cb0f9b98fb0af7f3f5ebb4d49948998
r2k <r2kodepl@gmail.com>
2023-03-24 17:25:10 +0100

    feat: basic authentication

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,53 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import * as argon from 'argon2';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
-  signup() {
-    return { msg: 'Sign UP' };
+  async signup(dto: AuthDto) {
+    const { email, password } = dto;
+    const hash = await argon.hash(password);
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email,
+          hash,
+        },
+        select: {
+          email: true,
+          createdAt: true,
+          firstName: true,
+          lastName: true,
+        },
+      });
+
+      return user;
+    } catch (error) {
+      if (error.code === 'P2002') {
+        throw new ForbiddenException('Credentials taken');
+      }
+    }
   }
 
-  signin() {
-    return { msg: 'Sign IN' };
+  async signin(dto: AuthDto) {
+    const { email, password } = dto;
+
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (!user) throw new ForbiddenException('Incorrect Credentials');
+
+    const passMatch = await argon.verify(user.hash, password);
+
+    if (!passMatch) throw new ForbiddenException('Incorrect credentials');
+
+    delete user.hash;
+
+    return user;
   }
 }
